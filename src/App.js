7bdd9574d03ebc597128ad5203877b6a45a26edd
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-12-19 20:11:09 +0530

    CHG | Context API

--- src/App.js
+++ src/App.js
@@ -23,10 +23,14 @@ import {
   LineChart
 } from './pages';
 
+import { useStateContext } from './contexts/ContextProvider';
+
 import './App.css';
 
 function App() {
-  const activeMenu = true;
+
+  const { activeMenu } = useStateContext();
+
   return (
     <div>
       <BrowserRouter>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ContextProvider } from './contexts/ContextProvider'
 // import reportWebVitals from './reportWebVitals';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,7 +11,12 @@ import App from './App';
 //     <App />
 //   </React.StrictMode>
 // );
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <ContextProvider>
+        <App />
+    </ContextProvider>, 
+    document.getElementById('root')
+);
 
 
 // If you want to start measuring performance in your app, pass a function
