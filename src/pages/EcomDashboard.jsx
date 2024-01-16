bcc0e3ffd53f3c8bac24c02cc19530bc52f394f2
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2024-01-16 22:46:05 +0530

    CHG | Use context api and change theme color and mode

--- src/pages/EcomDashboard.jsx
+++ src/pages/EcomDashboard.jsx
@@ -12,6 +12,8 @@ import { useStateContext } from "../contexts/ContextProvider";
 import { GoDot } from "react-icons/go";
 
 const EcomDashboard = () => {
+  const { currentColor } = useStateContext();
+
   return (
     <div className="mt-24">
       <div className="flex flex-wrap lg:flex-nowrap justify-center ">
@@ -26,7 +28,7 @@ const EcomDashboard = () => {
           <div className="mt-6">
             <Button
               color="white"
-              bgColor="blue"
+              bgColor={currentColor}
               text="Download"
               borderRadius="10px"
               size="md"
@@ -98,19 +100,19 @@ const EcomDashboard = () => {
 
               <div className="mt-5">
                 <SparkLine
-                  currentColor="blue"
+                  currentColor={currentColor}
                   id="line-sparkline"
                   type="Line"
                   height="80px"
                   width="250px"
                   data={SparklineAreaData}
-                  color="blue"
+                  color={currentColor}
                 />
               </div>
               <div className="mt-10">
                 <Button
                   color="white"
-                  bgColor="blue"
+                  bgColor={currentColor}
                   text="Download Report"
                   borderRadius="10px"
                 />
